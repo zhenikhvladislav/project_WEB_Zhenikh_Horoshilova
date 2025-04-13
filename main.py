4955ed639b13ccafbdc1d4b48143348a8af7208f
koyokan <horosilovasena@gmail.com>
2025-04-13 15:46:23 +0300

    что-то пока добавляю

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 from flask import Flask, render_template, redirect
-from flask_login import LoginManager, login_user
+from flask_login import LoginManager, login_user, logout_user, login_required
 from forms.user import RegisterForm, LoginForm
 from data import db_session
 from data.users import User
-
-# Импорт всех нужных классов и модулей
+from flask_login import current_user
 
 app = Flask(__name__)
-# Инициализирую LoginManager
 login_manager = LoginManager()
 login_manager.init_app(app)
 app.config['SECRET_KEY'] = 'your_secret_key'
 
 
-# Запуск сервера
 def main():
     db_session.global_init("db/users.db")
     app.run(port=8080, host='127.0.0.1', debug=True)
 
 
-# Функция получения пользователя
 @login_manager.user_loader
 def load_user(user_id):
     db_sess = db_session.create_session()
@@ -27,7 +23,7 @@ def load_user(user_id):
 
 
 @app.route('/register', methods=['GET', 'POST'])
-def register():  # вместо reqister
+def register():
     form = RegisterForm()
     if form.validate_on_submit():
         db_sess = db_session.create_session()
@@ -39,8 +35,8 @@ def register():  # вместо reqister
         user.set_password(form.password.data)
         db_sess.add(user)
         db_sess.commit()
-        login_user(user, remember=True)  # Автоматически авторизуем пользователя после регистрации
-        return redirect('/resume')  # Перенаправляем на страницу создания резюме
+        login_user(user, remember=True)
+        return redirect('/resume')
     return render_template('register.html', title='Регистрация', form=form)
 
 
@@ -52,9 +48,16 @@ def login():
         user = db_sess.query(User).filter(User.email == form.email.data).first()
         if user and user.check_password(form.password.data):
             login_user(user, remember=True)
-            return redirect('/resume')  # Перенаправляем на страницу создания резюме
+            return redirect('/resume')
         return render_template('login.html', title="Неправильный пароль", form=form)
     return render_template('login.html', title="Авторизация", form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/')
 
 
 @app.route('/resume')
@@ -65,6 +68,13 @@ def resume():
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', user=current_user)
 
 
 if __name__ == '__main__':
